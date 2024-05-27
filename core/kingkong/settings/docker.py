88c040465f2b2e47773f20d9fe48e14e12c80c92
Vitor Santanna <vitor_anna@id.uff.br>
2024-05-27 11:53:17 -0300

    codestyle and pre-commit

--- core/kingkong/settings/docker.py
+++ core/kingkong/settings/docker.py
@@ -1,5 +1,3 @@
-if IN_DOCKER:
+if IN_DOCKER:  # noqa
     print("IN_DOCKER is True")
-    assert MIDDLEWARE[:1] == [
-        'django.middleware.security.SecurityMiddleware'
-    ]
+    assert MIDDLEWARE[:1] == ["django.middleware.security.SecurityMiddleware"]  # noqa
