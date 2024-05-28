f71d78a89814a6e59270f0d7fafef72a7c9b9868
Vitor Santanna <95882160+vitorsantanna2@users.noreply.github.com>
2024-05-28 08:37:26 -0300

    Update base.py

--- core/kingkong/settings/base.py
+++ core/kingkong/settings/base.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import os
-
 # Application definition
 
 INSTALLED_APPS = [
