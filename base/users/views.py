35879997015a61d6f267eae8337df1c19cf5518c
Victor Vasconcellos <Victorvasconcellos42@gmail.com>
2024-07-21 20:50:13 -0300

    feat: add news views

--- base/users/views.py
+++ base/users/views.py
@@ -7,10 +7,4 @@ def login(request):
 	return render(request, "login.html")
 
 def register(request):
-	if request.method == "GET":
-		return render(request, "register.html")
-	else:
-		username = request.POST.get("username")
-		password = request.POST.get("password")
-		email = request.POST.get("email")
-		return HttpResponse(f"User {username} registered with {email} email")
+	return render(request, "register.html")
