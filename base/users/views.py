daeaf342f95e8debae59054f21890747172a7ddf
Victor Vasconcellos <Victorvasconcellos42@gmail.com>
2024-07-22 14:30:42 -0300

    fix: remove cache in develop environment

--- base/users/views.py
+++ base/users/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 
@@ -7,4 +8,5 @@ def login(request):
 	return render(request, "login.html")
 
 def register(request):
-	return render(request, "register.html")
+	timestamp = datetime.now().timestamp()
+	return render(request, "register.html", {"timestamp": timestamp})
