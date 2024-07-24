535a838c6f57a4e7569f62f0b572b43d8ac31127
Victor Vasconcellos <Victorvasconcellos42@gmail.com>
2024-07-24 13:10:43 -0300

    feat: add news paths

--- base/users/urls.py
+++ base/users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path("login/", views.login, name="login"),
+	path("login/", views.login_page, name="login"),
 	path("register/", views.register, name="register"),
+	path("home/", views.home_page, name="home"),
 ]
