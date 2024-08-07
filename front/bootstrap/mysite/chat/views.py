73d41bab4f005a16d27dee1e66628a313ff93158
psydenst <psydenst.dev@gmail.com>
2024-08-07 15:15:34 -0300

    integration js-py

--- front/bootstrap/mysite/chat/views.py
+++ front/bootstrap/mysite/chat/views.py
@@ -1,3 +1,4 @@
+# views.py 
 from django.shortcuts import render
 
 # Create your views here.
--- front/bootstrap/mysite/mysite/settings.py
+++ front/bootstrap/mysite/mysite/settings.py
@@ -148,3 +148,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'chat.User'
