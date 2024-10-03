c366ffe66a3ae124f4c90e4a9e32aa7b48bf0b19
nands93 <femmarques93+github@gmail.com>
2024-10-03 16:57:02 -0300

    collision done

--- mysite/main/game/ball.py
+++ mysite/main/game/ball.py
@@ -19,47 +19,19 @@ class Ball:
         self.x += self.speed_x
         self.y += self.speed_y
 
-    def collision(self):
+    def collision(self, player1, player2):
         if self.y <= 0 + self.radius or self.y >= self.height - self.radius:
             self.speed_y *= -1
         if self.x <= 0 + self.radius or self.x >= self.width - self.radius:
             self.reset_position()
+            return
+        if (self.x + self.radius >= player1.x_pos and self.x - self.radius <= player1.x_pos + player1.width) and \
+           (self.y - self.radius >= player1.y_pos and self.y - self.radius <= player1.y_pos + player1.height):
+            self.speed_x = abs(self.speed_x)
+        if (self.x + self.radius >= player2.x_pos and self.x - self.radius <= player2.x_pos + player2.width) and \
+           (self.y - self.radius >= player2.y_pos and self.y - self.radius <= player2.y_pos + player2.height):
+            self.speed_x = -abs(self.speed_x)
         
-        # if (self.x - self.radius <= player1.x + player1.width and
-        #     self.y >= player1.y and self.y <= player1.y + player1.height):
-        #     self.x = player1.x + player1.width + self.radius
-        #     print("Collision with player1")
-
-    #     if self.x <= 0 + self.radius:
-    #         # player2.score += 1
-    #         # if player2.score == game_score:
-    #         #     player2.rounds += 1
-    #         #     player1.score = 0
-    #         #     player2.score = 0
-    #         self.reset_position()
-    #         print(f"Collision with left wall: speed_x={self.speed_x}, speed_y={self.speed_y}")
-
-    #     if self.x >= self.width - self.radius:
-    #         # player1.score += 1
-    #         # if player1.score == game_score:
-    #         #     player1.rounds += 1
-    #         #     player1.score = 0
-    #         #     player2.score = 0
-    #         self.reset_position()
-    #         print(f"Collision with right wall: speed_x={self.speed_x}, speed_y={self.speed_y}")
-
-    #     if (self.x - self.radius <= player1.x + player1.width and
-    #         self.y >= player1.y and self.y <= player1.y + player1.height):
-    #         self.x = player1.x + player1.width + self.radius
-    #         self.speed_x *= -1
-    #         print(f"Collision with player1: speed_x={self.speed_x}")
-
-    #     if (self.x + self.radius >= player2.x and
-    #         self.y >= player2.y and self.y <= player2.y + player2.height):
-    #         self.x = player2.x - self.radius
-    #         self.speed_x *= -1
-    #         print(f"Collision with player2: speed_x={self.speed_x}")
-
     def reset_position(self):
         self.x = self.initial_x
         self.y = random.randint(50, self.height - 50)
