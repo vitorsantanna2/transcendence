4a2ff600f409736ac50ec5e22695ad0150f263da
Victor Vasconcellos <Victorvasconcellos42@gmail.com>
2024-11-15 19:19:51 -0300

    fix: templates problem and path error

--- mysite/users/urls.py
+++ mysite/users/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
 	path("login/", views.login_page, name="login"),
 	path("register/", views.register, name="register"),
-	path("home/", views.home_page, name="home"),
 ]
