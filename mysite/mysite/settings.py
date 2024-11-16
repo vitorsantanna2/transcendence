c9bf1ce3a3dbb6f90be97e1712fdfcd0f89eef43
nands93 <femmarques93+github@gmail.com>
2024-11-15 23:16:25 -0300

    fixed db port

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
        'USER': os.getenv('POSTGRES_USER'),
        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
        'HOST': os.getenv('DB_HOST'),
-       'PORT': os.getenv('DB_PORT'),
+       'PORT': 5432
    }
 }
 
