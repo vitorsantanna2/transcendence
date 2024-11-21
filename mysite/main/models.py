4579aae84be9e4130ba86f8380ff0447afc027aa
nands93 <femmarques93+github@gmail.com>
2024-11-21 18:48:17 -0300

    changes

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -2,10 +2,6 @@ from django.db import models
 
 class Match(models.Model):
     game_id = models.CharField(max_length=50, primary_key=True)
-    # user1 = models.ForeignKey('auth.User', related_name='user1_matches', on_delete=models.CASCADE)
-    # user2 = models.ForeignKey('auth.User', related_name='user2_matches', on_delete=models.CASCADE)
-    # user1_score = models.IntegerField(default=0)
-    # user2_score = models.IntegerField(default=0)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
