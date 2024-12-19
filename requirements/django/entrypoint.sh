8ec40f053d05368579f3134530aaabbaefae3073
nands93 <femmarques93+github@gmail.com>
2024-12-19 10:23:15 -0300

    updated make migration to python3

--- requirements/django/entrypoint.sh
+++ requirements/django/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-python /var/www/django/manage.py makemigrations
-python /var/www/django/manage.py migrate
-python /var/www/django/manage.py collectstatic --noinput
+python3 /var/www/django/manage.py makemigrations
+python3 /var/www/django/manage.py migrate
+python3 /var/www/django/manage.py collectstatic --noinput
 
 #python ../mysite/manage.py runserver 0.0.0.0:8001
 daphne -b 0.0.0.0 -p 8001 mysite.asgi:application
