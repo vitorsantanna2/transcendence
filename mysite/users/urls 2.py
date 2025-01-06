f965a40ac2e03b98922aba5f884fef3ac58e43ad
psydenst <psydenst.dev@gmail.com>
2025-01-06 18:39:52 -0300

    ChatConsumers to db

--- mysite/users/urls 2.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("login/", views.login_page, name="login"),
-    path("twofa/", views.twoFactorAuth, name="twofa"),
-    path("register/", views.register, name="register"),
-]
